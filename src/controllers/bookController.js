5d3f5831ffd26b57d4f4bbe970c9f5eda823bd35
poonam sandip patel <sarbhadi@gmail.com>
2022-05-23 16:57:02 +0530

    aws

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bookModel = require("../models/BooksModel");
 const userModel = require("../models/userModel");
 const reviewModel = require("../models/reviewModel");
+const aws= require("aws-sdk")
 
 const isValid = function (value) {
   if (typeof value === "undefined" || value === null) return false;
@@ -13,6 +14,36 @@ const isValidRequestBody = function (body) {
   return Object.keys(body).length > 0;
 };
 
+
+aws.config.update({
+  accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+  secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+  region: "ap-south-1"
+})
+
+let uploadFile= async ( file) =>{
+ return new Promise( function(resolve, reject) {
+  // this function will upload file to aws and return the link
+  let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+
+  var uploadParams= {
+      ACL: "public-read",
+      Bucket: "classroom-training-bucket",  //HERE
+      Key: "xyz/" + file.originalname, //HERE 
+      Body: file.buffer
+  }
+ 
+
+  s3.upload( uploadParams, function (err, data ){
+      if(err) {
+          return reject({"error": err})
+      }
+      console.log(data)
+      console.log("file uploaded succesfully")
+      return resolve(data.Location)
+  })
+})
+}
 const createBook = async function (req, res) {
   try {
     const data = req.body;
